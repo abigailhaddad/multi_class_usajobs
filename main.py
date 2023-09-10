ca43497819855668211ae77070a8590d55b0b280
Abigail Haddad <abigail.haddad@gmail.com>
2023-09-10 01:24:19 -0400

    -with some selected combinations of roles that seem challenging

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ fetch_and_write_out_historical()
 current_data = do_multi_labels()
 clustered_data = do_the_clustering()
 summary_df = frequencies_wordcloud()
+
+# challenging!
+
+challenges= ["Data Scientist, Data Analyst, Software Engineer", 
+             'Geospatial Analyst, Remote Sensing Specialist, Software Developer',
+             'Data Scientist, Data Analyst, Business Intelligence Analyst, Machine Learning Engineer, Artificial Intelligence Specialist, Data Engineer',
+             'Application Developer, Software Engineer, Cloud Engineer']
+
+challenge_summary = summary_df.loc[summary_df['occupation_str'].isin(challenges)]
